35fb0cd17744ccd3a5ab80c3f5bf8cf2b06856b5
kirill <zinchenkokir@gmail.com>
2023-08-16 14:09:26 +0300

    16.08

---
+++ GameBloc/GameBloc/Controllers/AddGameController.cs
@@ -0,0 +1,34 @@
+﻿using GameBloc.Models.Entity;
+using GameBloc.Models.Setvices.Game.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameBloc.Controllers
+{
+    public class AddGameController : Controller
+    {
+        [HttpGet]
+        public IActionResult AllGame() => View();
+
+        [HttpGet]
+        public IActionResult AddGame(int e)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddGame()
+        {
+            var context = Request.HttpContext;
+            var game = new Game();
+            game.Name = context.Request.Form["name"];
+            game.Genre = context.Request.Form["genre"];
+            game.Stylistics = context.Request.Form["stylistics"];
+            game.YearRelease = context.Request.Form["yearRelease"];
+            game.Author = context.Request.Form["author"];
+            game.Feelings = context.Request.Form["feelings"];
+            var service = context.RequestServices.GetService<IAddGameService>();
+            service.AddGame(game);
+            return RedirectPermanent("~/AddGame/AllGame");
+        }
+    }
+}
--- GameBloc/GameBloc/Program.cs
+++ GameBloc/GameBloc/Program.cs
@@ -1,5 +1,11 @@
+using GameBloc.Models.Entity;
+using GameBloc.Models.Setvices.Game;
+using GameBloc.Models.Setvices.Game.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IAddGameService, AddGameService>();
+builder.Services.AddTransient<IAllGame, AllGame>();
 var app = builder.Build();
 
 app.MapControllerRoute(
